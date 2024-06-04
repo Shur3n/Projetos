63a1ea7b6a299061ab2c7f929dce69766910563b
shur3n <contatolucas2704@gmail.com>
2024-06-04 14:58:54 -0300

    Nova pasta criada

---
+++ Projeto 1/index.js
@@ -0,0 +1,26 @@
+let Nome = "Lucas"
+let XP = "10500"
+const notificacaonome = "O Herói de nome "
+const notificacaonivel =  " Está no Rank " 
+
+
+
+if(XP <= 1000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Ferro")
+}else if(XP > 1001 && XP <= 2000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"bronze")
+}else if(XP > 2001 && XP <= 5000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Prata")
+}else if(XP > 5001 && XP <= 7000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Ouro")
+}else if(XP > 7001 && XP <= 8000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Platina")
+}else if(XP > 8001 && XP <= 9000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Ascendente")
+}else if(XP > 9001 && XP <= 10000){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Imortal")
+}else if(XP > 10001){
+    console.log("o Herói de nome " +Nome+notificacaonivel+"Radiant")
+}                  
+
+
